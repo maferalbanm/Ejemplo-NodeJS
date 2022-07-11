1cfc09baf7268d82d6aa75d8e2699bfb73bd4f0a
maferalbanm <maferalbanm@gmail.com>
2022-07-10 23:35:10 -0500

    make it better

---
+++ index.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const app=express();
+const port = 3000
+app.use(express.json());
+app.use(express.static('public'));
+app.set('nombreApp','Aplicacion para manejo de gastos SRI');
+console.log(app.get('nombreApp'));
+
+app.listen(app.get('puerto'), ()=>{
+    console.log('Nombre de la App:',app.get('nombreApp'));
+   })
+  
+app.get('/misitio', (req,res)=>{
+ res.send(`<Body background="https://img.freepik.com/vector-gratis/fondo-rosa-acuarela_1082-131.jpg?w=2000">
+    <h1 align="center"> Bienvenido a nuestro sitio web</h1>
+    <hr><br><br>
+    <h1 align="center"><b>Opciones:</b></h1> <br>
+    <a href="/misitio/gastos"><h2 align="center">Gastos</h2></a>
+    <br>
+    <a href="/misitio/about"><h2 align="center">Acerca de nosotros</h2></a>
+    </Body`);
+});
+app.get('/misitio/gastos', (req,res)=>{
+    res.json(
+    {
+    gasto:'Salud',
+    monto:14575.60,
+    informacion:'Corresponde a consultas medicas, pagos de seguros, medicinas'
+    }
+    );
+});
+
+app.get('/misitio/about', (req,res)=>{
+    res.send(`<Body background="https://img.freepik.com/vector-gratis/fondo-rosa-acuarela_1082-131.jpg?w=2000">
+    <h1 align="center"> Acerca de nosotros</h1>
+    <hr> <br>
+    <h2><b>Integrantes:</b> Maria Fernanda Alban, Bryan Gualoto, Anthony Saritama. </h2>
+    <h2><b>Materia: </b>Plataformas Web</h2>
+    <h2><b>Tema: </b>Google Cloud Platform </h2>
+    </Body`);
+});
+
+app.listen(port, ()=>{
+ console.log('Servidor escuchando en el puerto ' + port);
+})
